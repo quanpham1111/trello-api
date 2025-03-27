856fb91f8e683f0c99efbd1dde9cfa6fd5cd0717
Nguyen Quan <nguyenquan10k4@gmail.com>
2025-03-27 22:26:15 +0700

    validation with joi

--- src/routes/v1/boardRoute.js
+++ src/routes/v1/boardRoute.js
@@ -1,15 +1,12 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
-
+import { boardValidation } from '~/validations/boardValidation'
 const Router = express.Router()
 
 Router.route('/')
   .get((req, res) => {
     res.status(StatusCodes.OK).json({ message: 'GET: API get list board'})
   })
-  .post((req, res) => {
-    res.status(StatusCodes.CREATED).json({ message: 'POST: API create new board' })
+  .post(boardValidation.createNew)
 
-  })
-
-export const boardRoutes = Router
+export const boardRoute = Router
